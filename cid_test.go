b41162260a2b73e7a05c15801882068c8ba666fe
Fritz Schneider <fritz@protocol.ai>
2018-07-11 11:21:16 -1000

    fix typo

--- cid_test.go
+++ cid_test.go
@@ -409,6 +409,6 @@ func TestJsonRoundTrip(t *testing.T) {
 	var actual2 Cid
 	err = json.Unmarshal(enc, &actual2)
 	if !(*exp).Equals(&actual2) {
-		t.Fatal("cids not equal for *Cid")
+		t.Fatal("cids not equal for Cid")
 	}
 }
